c50c81f806c2eab735e4fa76aa4dd303df715e9d
Dionysis <dionysisterziou@gmail.com>
2026-01-25 20:55:05 +0200

    Add lesson 01 assertions

---
+++ lesson_01_assertions.py
@@ -0,0 +1,7 @@
+status_code = 200
+response_time_ms = 320
+
+assert status_code == 200
+assert response_time_ms < 500
+
+# Το assert δηλώνει τι περιμένουμε να ισχύει σε ένα test. Αν δεν ισχύει, το test αποτυγχάνει.
