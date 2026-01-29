35c91e9c3a220c2e30781e4427f50372c4ded410
Dionysis <dionysisterziou@gmail.com>
2026-01-29 22:19:20 +0200

    Use parametrize

--- test_smoke.py
+++ test_smoke.py
@@ -31,3 +31,14 @@ import pytest
 #     payment = {"amount": 0, "status": "SUCCESS"}
 #     with pytest.raises(AssertionError):
 #         validate_payment(payment)
+
+
+# Μάθημα 14 - @pytest.mark.parametrize
+def validate_payment(payment):
+    assert payment["amount"] > 0, "Amount must be positive"
+    assert payment["status"] == "SUCCESS", "Status must be SUCCESS"
+
+
+@pytest.mark.parametrize("payment", [{"amount": 1, "status": "SUCCESS"}, {"amount": 50, "status": "SUCCESS"}, {"amount": 999, "status": "SUCCESS"}])
+def test_valid_payments(payment):
+    validate_payment(payment)
