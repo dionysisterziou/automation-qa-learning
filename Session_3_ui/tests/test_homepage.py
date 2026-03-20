0e0cda17a2027b0530527b556155dd097b077d2f
Dionysis <dionysisterziou@gmail.com>
2026-03-20 17:11:09 +0200

    Improve redirect test with semantic assertion on IANA page

--- Session_3_ui/tests/test_homepage.py
+++ Session_3_ui/tests/test_homepage.py
@@ -9,5 +9,6 @@ def test_homepage_has_expected_title(homepage: Page):
 def test_homepage_link_redirects_to_iana(homepage: Page):
     homepage.get_by_text("Learn more").click()
     homepage.wait_for_url("**iana.org**")
+    expect(homepage.get_by_role("heading", name="Example Domains")).to_be_visible()
 
     assert "iana.org" in homepage.url
