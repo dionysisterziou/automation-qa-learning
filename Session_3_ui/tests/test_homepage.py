40fa2e1dbc4d5c284e476d7f7e1938470c59067f
Dionysis <dionysisterziou@gmail.com>
2026-03-21 21:42:09 +0200

    Refactor UI: keep only user-visible assertion(heading)

--- Session_3_ui/tests/test_homepage.py
+++ Session_3_ui/tests/test_homepage.py
@@ -2,7 +2,6 @@ from playwright.sync_api import Page, expect
 
 
 def test_homepage_has_expected_title(homepage: Page):
-    assert homepage.title() == "Example Domain"
     expect(homepage.get_by_role("heading", name="Example Domain")).to_be_visible()
 
 
