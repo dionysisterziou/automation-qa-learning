fd9497fe255bbbc1ff9c137c9fa4216cdf5a7be1
Dionysis <dionysisterziou@gmail.com>
2026-03-22 12:24:47 +0200

    Refactor UI tests: replace duplicate test with parametrized redirect test

--- Session_3_ui/tests/test_homepage.py
+++ Session_3_ui/tests/test_homepage.py
@@ -1,14 +1,19 @@
+import pytest
 from playwright.sync_api import Page, expect
-from test_data import LEARN_MORE_TEXT, IANA_HEADING, HOMEPAGE_HEADING
+from test_data import  HOMEPAGE_HEADING, LEARN_MORE_TEXT, IANA_HEADING
 
 
 def test_homepage_has_expected_title(homepage: Page):
     expect(homepage.get_by_role("heading", name=HOMEPAGE_HEADING)).to_be_visible()
 
 
-def test_homepage_link_redirects_to_iana(homepage: Page):
-    homepage.get_by_text(LEARN_MORE_TEXT).click()
+@pytest.mark.parametrize(
+    "link_text, expected_heading",
+    [
+        (LEARN_MORE_TEXT, IANA_HEADING)
+    ]
+)
+def test_link_redirect(homepage: Page, link_text, expected_heading):
+    homepage.get_by_text(link_text).click()
     homepage.wait_for_url("**iana.org**")
-    expect(homepage.get_by_role("heading", name=IANA_HEADING)).to_be_visible()
-
-    assert "iana.org" in homepage.url
+    expect(homepage.get_by_role("heading", name=expected_heading)).to_be_visible()
