8ab7ae131da2978e5fce15ed3b29466c79ad233a
Dionysis <dionysisterziou@gmail.com>
2026-03-22 12:03:06 +0200

    Refactor UI tests: extract test data into constants

---
+++ Session_3_ui/test_data.py
@@ -0,0 +1,3 @@
+LEARN_MORE_TEXT = "Learn more"
+IANA_HEADING = "Example Domains"
+HOMEPAGE_HEADING = "Example Domain"
--- Session_3_ui/tests/test_homepage.py
+++ Session_3_ui/tests/test_homepage.py
@@ -1,13 +1,14 @@
 from playwright.sync_api import Page, expect
+from test_data import LEARN_MORE_TEXT, IANA_HEADING, HOMEPAGE_HEADING
 
 
 def test_homepage_has_expected_title(homepage: Page):
-    expect(homepage.get_by_role("heading", name="Example Domain")).to_be_visible()
+    expect(homepage.get_by_role("heading", name=HOMEPAGE_HEADING)).to_be_visible()
 
 
 def test_homepage_link_redirects_to_iana(homepage: Page):
-    homepage.get_by_text("Learn more").click()
+    homepage.get_by_text(LEARN_MORE_TEXT).click()
     homepage.wait_for_url("**iana.org**")
-    expect(homepage.get_by_role("heading", name="Example Domains")).to_be_visible()
+    expect(homepage.get_by_role("heading", name=IANA_HEADING)).to_be_visible()
 
     assert "iana.org" in homepage.url
