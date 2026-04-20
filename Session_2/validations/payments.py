4a94a9a100bd6f8f4c5a636d9c34393915fd0354
Dionysis <dionysisterziou@gmail.com>
2026-04-20 21:43:39 +0300

    remove stale and unrelated api pracice files

--- Session_2/validations/payments.py
+++
@@ -1,7 +0,0 @@
-def validate_payment(payment):
-    assert payment["amount"] > 0, "Amount must be positive"
-    assert payment["status"] == "SUCCESS", "Status must be SUCCESS"
-
-
-def validate_payment_currency(payment):
-    assert payment["currency"] == "EUR", "Currency must be EUR"
--- Session_2/validations/posts.py
+++
@@ -1,3 +0,0 @@
-def validate_post_1(data):
-    assert data["id"] == 1, "Post id should be 1"
-    assert data["userId"] == 1, "User id should be 1"
